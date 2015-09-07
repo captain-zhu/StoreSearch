d7ca81e775aa9a1a7802d4f37a1a2a4f8a5bc73d
captain-zhu <shibaier@foxmail.com>
2015-09-07 15:59:46 +0800

    Initial Commit

---
+++ StoreSearch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StoreSearch
+//
+//  Created by zhu yongxuan on 15/9/7.
+//  Copyright (c) 2015年 zhu yongxuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
