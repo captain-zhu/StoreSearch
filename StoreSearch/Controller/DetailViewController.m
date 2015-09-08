cdd5d7bcf1e281fe777af83308bd68b73bbc9f8d
captain-zhu <shibaier@foxmail.com>
2015-09-08 22:22:14 +0800

    加入了detailviewcontroller

---
+++ StoreSearch/Controller/DetailViewController.m
@@ -0,0 +1,41 @@
+//
+//  DetailViewController.m
+//  StoreSearch
+//
+//  Created by zhu yongxuan on 15/9/8.
+//  Copyright (c) 2015年 zhu yongxuan. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)dealloc
+{
+    NSLog(@"dealloc %@", self);
+}
+
+#pragma mark - Action
+
+- (IBAction)close:(id)sender
+{
+    [self willMoveToParentViewController:nil];
+    [self.view removeFromSuperview];
+    [self removeFromParentViewController];
+}
+
+@end
