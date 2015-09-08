1ad2624ef78e97cb9022c5f0175ccf56d00f7439
captain-zhu <shibaier@foxmail.com>
2015-09-08 22:36:55 +0800

    制作了detailviewcontroller的xib

--- StoreSearch/Controller/DetailViewController.h
+++ StoreSearch/Controller/DetailViewController.h
@@ -10,4 +10,12 @@
 
 @interface DetailViewController : UIViewController
 
+@property (nonatomic, weak) IBOutlet UIView *popupView;
+@property (nonatomic, weak) IBOutlet UIImageView *artworkImageView;
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *artistNameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *kindLabel;
+@property (nonatomic, weak) IBOutlet UILabel *genreLabel;
+@property (nonatomic, weak) IBOutlet UIButton *priceButton;
+
 @end
